663aa360479eff333e3867c5d868b4edfd7e7173
Dino Stancic <dinostancic7@gmail.com>
2021-02-25 12:27:36 +0100

    Add routes for getting one or more data for created queries

---
+++ src/routes/routes.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("../db/db");
+const router = express.Router();
+
+// GET ALL USERS
+router.get("/", async (req, res, next) => {
+  try {
+    let results = await db.all();
+    res.json(results);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+// GET ONE USER
+router.get("/:id", async (req, res, next) => {
+  try {
+    let results = await db.one(req.params.id);
+    res.json(results);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
