30df158fa649268d46560f19069b9557c231aedf
Dino Stancic <dinostancic7@gmail.com>
2021-02-25 12:21:55 +0100

    Use express and add start first server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Initial log");
+export {};
+const express = require("express");
+const apiRouter = require("./routes/routes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/", apiRouter);
+
+app.listen(process.env.PORT || "3002", () => {
+  console.log(`Server is running on port: ${process.env.PORT || "3002"}`);
+});
