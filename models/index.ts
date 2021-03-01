6f284b55f42f18c4728c5b4ff16af3757b7cf6bd
Dino Stancic <dinostancic7@gmail.com>
2021-03-01 18:22:46 +0100

    Add dotenv require

--- models/index.ts
+++ models/index.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config();
 
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
