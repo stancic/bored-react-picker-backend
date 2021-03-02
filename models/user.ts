4cadd10377d1e5397140e0c61bfa46f1ed43e854
Dino Stancic <dinostancic7@gmail.com>
2021-03-02 14:05:14 +0100

    Export UserCreate class with class-validators and add RegExe's

--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,37 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "./index";
+import {
+  IsDefined,
+  Length,
+  MaxLength,
+  Matches,
+  MinLength,
+} from "class-validator";
+
+const testUsernameRegEx = /^(?=.{4,31}$)(?![_\W])(?![_\W]{2})\w+(?<!(_{1})[_\W])$/;
+const testEmailRegEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+const testPasswordRegEx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/;
+
+export class UserCreate {
+  id: string;
+
+  @IsDefined()
+  @MaxLength(32, { message: "Username too long" })
+  @Matches(testUsernameRegEx, { message: "Username is not in valid format." })
+  username: string;
+
+  @IsDefined()
+  @Matches(testEmailRegEx, { message: "Email is not in valid format." })
+  email: string;
+
+  @IsDefined()
+  @MinLength(6, { message: "Password too short" })
+  @Matches(testPasswordRegEx, {
+    message:
+      "Password too weak (It needs have one capitalized letter and at least one number).",
+  })
+  password: string;
+}
 
 export const User = sequelize.define<any, any>(
   "User",
