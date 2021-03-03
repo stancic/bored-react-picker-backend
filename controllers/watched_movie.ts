287d619213588b573fbb90181c2b4ec872f68021
Dino Stancic <dinostancic7@gmail.com>
2021-03-03 11:31:22 +0100

    Add add movies to watched list request

--- controllers/watched_movie.ts
+++ controllers/watched_movie.ts
@@ -3,8 +3,43 @@ import { plainToClass } from "class-transformer";
 import { validate } from "class-validator";
 import { v4 as uuidv4 } from "uuid";
 
-// GET ALL WATCHED MOVIES
 export const getAll = async (request, response) => {
   const watchedMovies = await WatchedMovie.findAll();
   response.json(watchedMovies);
+};
+
+export const addMovieToWatchedMovies = async (request, response) => {
+  const body = request.body;
+  const watchedMovieBodyToClass = plainToClass(WatchedMovieCreate, body);
+
+  validate(watchedMovieBodyToClass).then(async (errors) => {
+    if (errors.length > 0) {
+      response.status(400).json({
+        message: "Validation failed",
+        errors,
+      });
+    } else {
+      let watchedMovieId = uuidv4();
+      const watchedMovie = {
+        id: watchedMovieId,
+        movieId: body.movieId,
+        userId: body.userId,
+      };
+      try {
+        let result = await WatchedMovie.create({
+          id: watchedMovie.id,
+          movieId: watchedMovie.movieId,
+          userId: watchedMovie.userId,
+        });
+        response.status(200).json({
+          message: "Movie added to watched movies",
+          result,
+        });
+      } catch (error) {
+        response.status(500).json({
+          message: "Movie is already on the list",
+        });
+      }
+    }
+  });
 };
