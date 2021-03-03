8978fa41cfd7e34e35e306780b08403b4b7511cf
Dino Stancic <dinostancic7@gmail.com>
2021-03-03 11:12:24 +0100

    Use watched movie router

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import * as express from "express";
 const userRoutes = require("./routes/user");
+const watchedMovieRoutes = require("./routes/watched_movie");
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 
 app.use("/api", userRoutes);
+app.use("/api", watchedMovieRoutes);
 app.listen(process.env.PORT || "3002", () => {
   console.log(`Server is running on port: ${process.env.PORT || "3002"}`);
 });
