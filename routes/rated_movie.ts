d85076c4126c83fb9487f5a4d253ae136cdacb4c
Dino Stancic <dinostancic7@gmail.com>
2021-03-03 12:09:13 +0100

    Add post request for movie rating

--- routes/rated_movie.ts
+++ routes/rated_movie.ts
@@ -3,5 +3,6 @@ import * as RatedMovieController from "../controllers/rated_movie";
 const router = express.Router();
 
 router.get("/ratedMovies/all", RatedMovieController.getAll);
+router.post("/ratedMovies/rate", RatedMovieController.rateMovie);
 
 module.exports = router;
