6ccc5c455513f926074ca4a9e3225d716c2c05d9
Dino Stancic <dinostancic7@gmail.com>
2021-03-08 16:23:04 +0100

    Remove unused variables

--- middlewares/authorization.ts
+++ middlewares/authorization.ts
@@ -6,7 +6,7 @@ export const authorize = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers.authorization;
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     const token = authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET, (error, user) => {
+    jwt.verify(token, process.env.SECRET, (error) => {
       if (error) {
         res.sendStatus(403);
       }
