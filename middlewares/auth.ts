f9efb0c42c0a3bfb420eaf778fe48511885c8128
Dino Stancic <dinostancic7@gmail.com>
2021-03-08 16:19:07 +0100

    Add middleware for authorization

---
+++ middlewares/auth.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction, request } from "express";
+import * as jwt from "jsonwebtoken";
+require("dotenv").config();
+
+export const authorize = (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers.authorization;
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET, (error, user) => {
+      if (error) {
+        res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
