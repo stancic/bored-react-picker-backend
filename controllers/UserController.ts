610647c284259066249e3fd9fc49af3063248f96
Dino Stancic <dinostancic7@gmail.com>
2021-03-08 15:36:51 +0100

    Add middleware for validating body

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import { User, UserCreate } from "../models/UserModel";
-import { plainToClass } from "class-transformer";
-import { validate } from "class-validator";
 import { v4 as uuidv4 } from "uuid";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
@@ -14,57 +12,46 @@ export class UserController {
   };
 
   signUp = async (req: Request, res: Response) => {
-    const body = req.body;
-    const userBodyToInstance = plainToClass(UserCreate, body);
+    const body: UserCreate = res.locals.body;
+    let userId = uuidv4();
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-    validate(userBodyToInstance).then(async (errors) => {
-      if (errors.length > 0) {
-        res.status(400).json({
-          message: "Validation failed.",
-          errors,
-        });
-      } else {
-        let userId = uuidv4();
-        const saltRounds = 10;
-        const passwordHash = await bcrypt.hash(body.password, saltRounds);
-
-        // If user exists with one username but different mail and vice versa
-        const userWithUsernameExists = await User.findOne({
-          where: { username: body.username },
-        });
-        const userWithEmailExists = await User.findOne({
-          where: { email: body.email },
-        });
-
-        const user = {
-          id: userId,
-          username: body.username,
-          email: body.email,
-          password: passwordHash,
-        };
-
-        if (userWithUsernameExists || userWithEmailExists) {
-          res.status(409).json({
-            message: "Username or email already exists",
-          });
-        } else {
-          try {
-            let result = await User.create({
-              id: user.id,
-              username: user.username,
-              email: user.email,
-              password: user.password,
-            });
-            res.status(200).json({
-              message: `User ${user.username} created`,
-              result,
-            });
-          } catch (error) {
-            res.sendStatus(500);
-          }
-        }
-      }
+    // If user exists with one username but different mail and vice versa
+    const userWithUsernameExists = await User.findOne({
+      where: { username: body.username },
     });
+    const userWithEmailExists = await User.findOne({
+      where: { email: body.email },
+    });
+
+    const user = {
+      id: userId,
+      username: body.username,
+      email: body.email,
+      password: passwordHash,
+    };
+
+    if (userWithUsernameExists || userWithEmailExists) {
+      res.status(409).json({
+        message: "Username or email already exists",
+      });
+    } else {
+      try {
+        let result = await User.create({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          password: user.password,
+        });
+        res.status(200).json({
+          message: `User ${user.username} created`,
+          result,
+        });
+      } catch (error) {
+        res.sendStatus(500);
+      }
+    }
   };
 
   login = async (req: Request, res: Response) => {
