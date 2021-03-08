495faa52897c50e42c88d6d06e5fbf03ad724182
Dino Stancic <dinostancic7@gmail.com>
2021-03-08 10:37:04 +0100

    Change controller to class

--- controllers/WatchedMovieController.ts
+++ controllers/WatchedMovieController.ts
@@ -3,43 +3,45 @@ import { plainToClass } from "class-transformer";
 import { validate } from "class-validator";
 import { v4 as uuidv4 } from "uuid";
 
-export const getAll = async (request, response) => {
-  const watchedMovies = await WatchedMovie.findAll();
-  response.json(watchedMovies);
-};
+export class WatchedMovieController {
+  getAll = async (request, response) => {
+    const watchedMovies = await WatchedMovie.findAll();
+    response.json(watchedMovies);
+  };
 
-export const addMovieToWatchedMovies = async (request, response) => {
-  const body = request.body;
-  const watchedMovieBodyToClass = plainToClass(WatchedMovieCreate, body);
+  addMovieToWatchedMovies = async (request, response) => {
+    const body = request.body;
+    const watchedMovieBodyToClass = plainToClass(WatchedMovieCreate, body);
 
-  validate(watchedMovieBodyToClass).then(async (errors) => {
-    if (errors.length > 0) {
-      response.status(400).json({
-        message: "Validation failed",
-        errors,
-      });
-    } else {
-      let watchedMovieId = uuidv4();
-      const watchedMovie = {
-        id: watchedMovieId,
-        movieId: body.movieId,
-        userId: body.userId,
-      };
-      try {
-        let result = await WatchedMovie.create({
-          id: watchedMovie.id,
-          movieId: watchedMovie.movieId,
-          userId: watchedMovie.userId,
+    validate(watchedMovieBodyToClass).then(async (errors) => {
+      if (errors.length > 0) {
+        response.status(400).json({
+          message: "Validation failed",
+          errors,
         });
-        response.status(200).json({
-          message: "Movie added to watched movies",
-          result,
-        });
-      } catch (error) {
-        response.status(500).json({
-          message: "Movie is already on the list",
-        });
+      } else {
+        let watchedMovieId = uuidv4();
+        const watchedMovie = {
+          id: watchedMovieId,
+          movieId: body.movieId,
+          userId: body.userId,
+        };
+        try {
+          let result = await WatchedMovie.create({
+            id: watchedMovie.id,
+            movieId: watchedMovie.movieId,
+            userId: watchedMovie.userId,
+          });
+          response.status(200).json({
+            message: "Movie added to watched movies",
+            result,
+          });
+        } catch (error) {
+          response.status(500).json({
+            message: "Movie is already on the list",
+          });
+        }
       }
-    }
-  });
-};
+    });
+  };
+}
