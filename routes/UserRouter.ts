ef5f2e61cb1999173f073b3d61cfff364de97748
Dino Stancic <dinostancic7@gmail.com>
2021-03-08 15:36:17 +0100

    Add middleware

--- routes/UserRouter.ts
+++ routes/UserRouter.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { UserController } from "../controllers/UserController";
+import { UserCreate } from "../models/UserModel";
+import { bodyValidation } from "../middlewares/middleware";
 const userRouter = express.Router();
 
 const userController = new UserController();
 userRouter.get("", userController.getAll);
-userRouter.post("/sign-up", userController.signUp);
+userRouter.post("/sign-up", bodyValidation(UserCreate), userController.signUp);
 userRouter.post("/login", userController.login);
 
 export default userRouter;
