ed4cc988157b336f646a3ea8a994282dcde92f8b
Dino Stancic <dinostancic7@gmail.com>
2021-03-10 11:02:38 +0100

    Use cors

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 import * as express from "express";
 import router from "./routes/index";
+import * as cors from "cors";
 
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", router);
 
 app.listen(process.env.PORT || "3002", () => {
