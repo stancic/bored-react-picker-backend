6ae0ca575b0fae04d3d16e772331e5a725710194
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 14:36:27 +0100

    Add next function and fix returns

--- src/controllers/FavoriteMovieController.ts
+++ src/controllers/FavoriteMovieController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import {
   FavoriteMovie,
   FavoriteMovieCreate,
@@ -12,13 +12,21 @@ export class FavoriteMovieController {
     this._favoriteMoviesService = favoriteMovieService;
   }
 
-  GetAllFavoritesAsync = async (req: Request, res: Response) => {
+  GetAllFavoritesAsync = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     const favoriteMovies = await this._favoriteMoviesService
       .FindAllFavoriteMoviesAsync;
     return res.status(200).json(favoriteMovies);
   };
 
-  GetFavoritesFromUserAsync = async (req: Request, res: Response) => {
+  GetFavoritesFromUserAsync = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     const userId = req.params.userid;
     const favoriteMoviesFromUser = await this._favoriteMoviesService.GetByUserIdAsync(
       userId
@@ -26,7 +34,11 @@ export class FavoriteMovieController {
     return res.status(200).json(favoriteMoviesFromUser);
   };
 
-  AddMovieToFavoriteMoviesAsync = async (req: Request, res: Response) => {
+  AddMovieToFavoriteMoviesAsync = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     const body: FavoriteMovieCreate = res.locals.body;
     let favoriteMovieId = uuidv4();
     const favoriteMovie = {
@@ -36,22 +48,26 @@ export class FavoriteMovieController {
     };
     try {
       let result = await this._favoriteMoviesService.PostAsync(favoriteMovie);
-      res.status(200).json({
+      return res.status(200).json({
         message: "Movie added to favorite movies",
         result,
       });
     } catch (error) {
-      res.status(500).json({
+      return res.status(500).json({
         message:
           "Movie is already on the list or you're doing something not allowed",
       });
     }
   };
 
-  RemoveFavoriteMovieAsync = async (req: Request, res: Response) => {
+  RemoveFavoriteMovieAsync = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     const id = req.params.id;
     const favoriteMovie = this._favoriteMoviesService.RemoveByMovieIdAsync(id);
-    res.json({
+    return res.status(204).json({
       message: "Movie removed from favorite movies",
       favoriteMovie,
     });
