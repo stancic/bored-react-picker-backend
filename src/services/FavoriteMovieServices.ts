deb4acd65ef246d9f754da9a20304ebd26023fcf
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 14:29:28 +0100

    Add favorite movie services for db work

---
+++ src/services/FavoriteMovieServices.ts
@@ -0,0 +1,24 @@
+import { injectable } from "tsyringe";
+import { FavoriteMovie } from "../models/FavoriteMovieModel";
+import { IMovie } from "../types/IMovie";
+
+@injectable()
+export default class FavoriteMovieService {
+  FindAllFavoriteMoviesAsync = async (): Promise<Array<IMovie>> => {
+    return FavoriteMovie.findAll();
+  };
+
+  GetByUserIdAsync = async (
+    userId: string
+  ): Promise<Array<IMovie> | IMovie> => {
+    return FavoriteMovie.findOne({ where: { userId: userId } });
+  };
+
+  PostAsync = async (movie: IMovie): Promise<IMovie> => {
+    return FavoriteMovie.create(movie);
+  };
+
+  RemoveByMovieIdAsync = async (id: string): Promise<number> => {
+    return FavoriteMovie.destroy({ where: { id: id } });
+  };
+}
