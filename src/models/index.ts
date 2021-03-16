32ed015aea2f43babad39af42efb734d58c1e118
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 14:16:06 +0100

    Import from env

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const config = {
   database: process.env.DATABASE_NAME || "bored_movie_picker",
-  username: process.env.USERNAME || "root",
+  username: process.env.DATABASE_USERNAME || "root",
   password: process.env.DATABASE_PASSWORD || "",
   dialect: process.env.DATABASE_DIALECT || "mysql",
 };
