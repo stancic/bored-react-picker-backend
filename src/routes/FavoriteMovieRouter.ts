da04e6c80efb4e992113eefd6ed3e5398170c935
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 14:29:41 +0100

    Use tsyringe to solve dependency injection and change method names

--- src/routes/FavoriteMovieRouter.ts
+++ src/routes/FavoriteMovieRouter.ts
@@ -3,22 +3,30 @@ import { FavoriteMovieController } from "../controllers/FavoriteMovieController"
 import { bodyValidation } from "../middlewares/middleware";
 import { authorize } from "../middlewares/authorization";
 import { FavoriteMovieCreate } from "../models/FavoriteMovieModel";
+import { container } from "tsyringe";
 const favoriteMovieRouter = express.Router();
 
-const favoriteMovieController = new FavoriteMovieController();
+const favoriteMovieController = container.resolve(FavoriteMovieController);
 
-favoriteMovieRouter.get("", authorize, favoriteMovieController.getAll);
+favoriteMovieRouter.get(
+  "",
+  authorize,
+  favoriteMovieController.GetAllFavoritesAsync
+);
 favoriteMovieRouter.get(
   "/:userid",
   authorize,
-  favoriteMovieController.getFromUser
+  favoriteMovieController.GetFavoritesFromUserAsync
 );
 favoriteMovieRouter.post(
   "",
   authorize,
   bodyValidation(FavoriteMovieCreate),
-  favoriteMovieController.addMovieToFavoriteMovies
+  favoriteMovieController.AddMovieToFavoriteMoviesAsync
 );
-favoriteMovieRouter.delete("/:id", favoriteMovieController.removeFavoriteMovie);
+favoriteMovieRouter.delete(
+  "/:id",
+  favoriteMovieController.RemoveFavoriteMovieAsync
+);
 
 export default favoriteMovieRouter;
