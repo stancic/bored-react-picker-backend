46cea5f4afc5fe9b84ec97045ff13d2d6985067b
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 15:12:39 +0100

    Use tsyringe to solve dependency injection and change method names

--- src/routes/RatedMovieRouter.ts
+++ src/routes/RatedMovieRouter.ts
@@ -1,17 +1,22 @@
 import * as express from "express";
+import { container } from "tsyringe";
 import { RatedMovieController } from "../controllers/RatedMovieController";
 import { authorize } from "../middlewares/authorization";
 import { bodyValidation } from "../middlewares/middleware";
 import { RatedMovieCreate } from "../models/RatedMovieModel";
 const ratedMovieRouter = express.Router();
 
-const ratedMovieController = new RatedMovieController();
-ratedMovieRouter.get("", authorize, ratedMovieController.getAll);
+const ratedMovieController = container.resolve(RatedMovieController);
+ratedMovieRouter.get(
+  "",
+  authorize,
+  ratedMovieController.GetAllRatedMoviesAsync
+);
 ratedMovieRouter.post(
   "",
   authorize,
   bodyValidation(RatedMovieCreate),
-  ratedMovieController.rateMovie
+  ratedMovieController.RateMovieAsync
 );
 
 export default ratedMovieRouter;
