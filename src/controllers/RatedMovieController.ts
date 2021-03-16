0b39c26eb4377ff2d195a066d33f789fd3e69855
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 15:13:06 +0100

    Use @injectable, solve dependencies and use services

--- src/controllers/RatedMovieController.ts
+++ src/controllers/RatedMovieController.ts
@@ -1,14 +1,27 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { RatedMovie, RatedMovieCreate } from "../models/RatedMovieModel";
 import { v4 as uuidv4 } from "uuid";
+import RatedMovieService from "../services/RatedMovieServices";
+import { injectable } from "tsyringe";
 
+@injectable()
 export class RatedMovieController {
-  getAll = async (req: Request, res: Response) => {
-    const ratedMovies = await RatedMovie.findAll();
-    res.json(ratedMovies);
+  private _ratedMovieService: RatedMovieService;
+
+  constructor(ratedMovieService: RatedMovieService) {
+    this._ratedMovieService = ratedMovieService;
+  }
+
+  GetAllRatedMoviesAsync = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const ratedMovies = await this._ratedMovieService.FindAllRatedMoviesAsync;
+    return res.status(200).json(ratedMovies);
   };
 
-  rateMovie = async (req: Request, res: Response) => {
+  RateMovieAsync = async (req: Request, res: Response, next: NextFunction) => {
     const body: RatedMovieCreate = res.locals.body;
     let ratedMovieId = uuidv4();
     const ratedMovie = {
@@ -18,18 +31,13 @@ export class RatedMovieController {
       userId: body.userId,
     };
     try {
-      let result = await RatedMovie.create({
-        id: ratedMovie.id,
-        rate: ratedMovie.rate,
-        movieId: ratedMovie.movieId,
-        userId: ratedMovie.userId,
-      });
-      res.status(200).json({
+      let result = await this._ratedMovieService.PostAsync(ratedMovie);
+      return res.status(200).json({
         message: "Movie rated",
         result,
       });
     } catch (error) {
-      res.status(500).json({
+      return res.status(500).json({
         message:
           "You've already rated this movie or you're doing something not allowed",
         ratedMovie,
