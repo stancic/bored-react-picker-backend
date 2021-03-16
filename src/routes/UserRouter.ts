408d1366b37fed066f01754eca895c98ffe1c051
Dino Stancic <dinostancic7@gmail.com>
2021-03-16 14:15:54 +0100

    Use tsyringe to solve dependency injection and change method names

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -3,11 +3,17 @@ import { UserController } from "../controllers/UserController";
 import { UserCreate } from "../models/UserModel";
 import { bodyValidation } from "../middlewares/middleware";
 import { authorize } from "../middlewares/authorization";
+import { container } from "tsyringe";
 const userRouter = express.Router();
 
-const userController = new UserController();
-userRouter.get("", authorize, userController.getAll);
-userRouter.post("/sign-up", bodyValidation(UserCreate), userController.signUp);
-userRouter.post("/login", userController.login);
+const userController = container.resolve(UserController);
+
+userRouter.get("", authorize, userController.GetAllUsersAsync);
+userRouter.post(
+  "/sign-up",
+  bodyValidation(UserCreate),
+  userController.SignupAsync
+);
+userRouter.post("/login", userController.LoginAsync);
 
 export default userRouter;
